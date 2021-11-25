5288953c61c29ca71cfb96b2799382bb64304380
Ivan Pilipović <pilipovicivan@yahoo.com>
2021-11-25 22:53:12 +0100

    Added selection sort.

---
+++ sorting algorithms/selection_sort.py
@@ -0,0 +1,12 @@
+# Time complexity O(n^2)
+
+def selection_sort(a_list):
+	for i in range(len(a_list)):
+		min_index = i
+		for j in range(i+1, len(a_list)):
+			if a_list[j] < a_list[min_index]:
+				min_index = j
+
+		a_list[i], a_list[min_index] = a_list[min_index], a_list[i]
+
+	return a_list
---
+++ sorting algorithms/tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+from selection_sort import selection_sort
+
+class SortingTests(unittest.TestCase):
+	test_list_1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+	test_list_2 = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+	test_list_3 = [1,4,2,3,19,18,5,6,20,17,11,10,7,8,12,14,9,13,15,16]
+
+	def test_selection_sort(self):
+		self.assertEqual(selection_sort(self.test_list_1), self.test_list_1)
+		self.assertEqual(selection_sort(self.test_list_2), self.test_list_1)
+		self.assertEqual(selection_sort(self.test_list_3), self.test_list_1)
+
+
+if __name__ == "__main__":
+	unittest.main()
